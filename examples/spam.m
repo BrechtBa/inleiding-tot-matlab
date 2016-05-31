6f2bfcec7b1298950c4bf4fc2484fb30ba3be20f
brecht <brecht.baeten@gmail.com>
2016-05-31 10:07:36 +0200

    Updated text with section on plotting

--- examples/spam.m
+++ examples/spam.m
@@ -1,7 +1,7 @@
 function val = spam(A,B,C,D)
 	% returns a a number as if the arguments were different digits in the number
 	%
-	% Arguments:
+	% Parameters:
 	% A: float, hundreds
 	% B: float, decades
 	% C: float, units
